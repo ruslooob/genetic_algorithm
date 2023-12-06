d9b65c3674dc39e4bc206e6b720f8b0588be4fb2
ruslooob <rm952576@gmail.com>
2023-12-06 14:03:43 +0300

    1 версия генетического алгоритма.
    Пока реализован функционал для чисел с плавающей точкой.

---
+++ src/main/java/com/ruslooob/Main.java
@@ -0,0 +1,8 @@
+package com.ruslooob;
+
+public class Main {
+    public static void main(String[] args) {
+        var geneticAlgorithm = new GeneticAlgorithmPerformer();
+        geneticAlgorithm.start();
+    }
+}
